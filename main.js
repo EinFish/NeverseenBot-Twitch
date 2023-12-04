4c3347e0c7cb37bcc680e5d074bef03b4c4d19f5
EinFish <finley.alster2@gmail.com>
2023-12-04 21:14:15 +0100

    fixed bug and added one command

--- main.js
+++ main.js
@@ -19,8 +19,23 @@ CLIENT.on("open", () => {
 	CLIENT.send(`PRIVMSG #${CONFIG.channel} :This is a sample message`);
 });
 
+
+/**
+ * 
+ * @param {string} str 
+ */
+function parseMessage(str) {
+	const raw = str.substring(str.indexOf("#"));
+	const i = raw.indexOf(":") - 1;
+
+	return {
+		channel: raw.substring(0, i),
+		message: raw.substring(i + 2)
+	}
+}
+
 CLIENT.on("message", event => {
-	const MESSAGE = event.toString();
+	const MESSAGE = (event.toString());
 
 	if (MESSAGE.startsWith("PING")) {
 		Log.append("Recieved PING, sending PONG...", { level: "WARN" });
@@ -36,11 +51,15 @@ CLIENT.on("message", event => {
 
 	if (/\btest\b/.test(MESSAGE)) {
 		console.log(/#[0-9_a-z]+\s/.exec(MESSAGE))
-		CLIENT.send(`PRIVMSG #${/#\w+\s/.exec(MESSAGE)} :HeyGuys <3 PartyTime`);
+		console.log(MESSAGE.split(":"))
+		CLIENT.send(`PRIVMSG ${/#\w+\s/.exec(MESSAGE)} :HeyGuys <3 PartyTime`);
 	}
 
-	if (/\!neverseenbot\b/.test(MESSAGE)) {
-		CLIENT.send(`PRIVMSG #${/#\w+\s/.exec(MESSAGE)} :Der NeverseenBot ist im Moment noch in Entwicklung. Für Mehr Infos bitte per Discord eine Direcknachricht an LeFish schreiben.`);
+	const LOL = parseMessage(MESSAGE);
+	console.log(JSON.stringify(LOL))
+
+	if (LOL.message.startsWith("!neverseenbot")) {
+		CLIENT.send(`PRIVMSG ${LOL.channel} :Der NeverseenBot ist im Moment noch in Entwicklung. Für Mehr Infos bitte per Discord eine Direcknachricht an LeFish schreiben.`);
 	}
 
 
